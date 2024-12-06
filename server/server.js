0ccac87186017f92a46749b77308933f9860b82c
Benjamin Brechter <benbrechter3@gmail.com>
2024-12-06 12:30:51 -0600

    work please

--- server/server.js
+++ server/server.js
@@ -7,10 +7,8 @@ const connectDB = require('./config/connections');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
 
 // Create uploads folder if it doesn't exist
 const uploadsDir = path.join(__dirname, 'uploads');
@@ -19,10 +17,8 @@ if (!fs.existsSync(uploadsDir)) {
     console.log('Uploads directory created at:', uploadsDir);
 }
 
-
 // Connect to MongoDB
 connectDB();
-
 
 // Middleware
 app.use(cors());
@@ -30,24 +26,19 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Serve uploaded files statically - use absolute path
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
 
 // Routes
 app.use('/api', routes);
 
-
 // Error handling
 app.use(errorHandler);
-
 
 // Add error logging for unhandled promises
 process.on('unhandledRejection', (error) => {
     console.error('Unhandled Rejection:', error);
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
