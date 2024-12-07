9ee087f196c5e3e43e3a0d5f5992d8bbf3e8e5f8
Benjamin Brechter <benbrechter3@gmail.com>
2024-12-07 17:52:25 -0600

    comment how to fix

--- server/config/connections.js
+++ server/config/connections.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+//I need to make this function 
+//https://stackoverflow.com/questions/21626896/nodejs-mongodb-native-find-all-documents
+
+
 const MONGODB_URI = 'mongodb://benbrechter3:W2O308Q15ALz6NOY@benbeejammin.7e5q1.mongodb.net/test'
 const connectDB = async () => {
   try {
